adb1dcc4951f3871cf2c61720bb6fb7b2a286028
Hamza HMITOU <hamzahmitou@gmail.com>
2023-08-19 04:22:58 +0300

    Fixed env issue

--- akitoai_backend/akitoai_backend/settings.py
+++ akitoai_backend/akitoai_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m&ijn-@%hh)x21d*+t_ppc0g*e+s#y%n4e)8jhd+$avjr+k7qh'
+SECRET_KEY = 'DJANGO_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
